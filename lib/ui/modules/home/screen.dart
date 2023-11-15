b2fc95a5f9f90cf6a2a69dd36726fe6552aa0136
ParimSsme <parima.ssme@gmail.com>
2023-11-15 09:53:09 +0430

    add home screen to tabs

--- lib/ui/modules/home/screen.dart
+++ lib/ui/modules/home/screen.dart
@@ -3,7 +3,6 @@ import 'package:provider/provider.dart';
 import 'package:sqflite_mvvm_design/providers/poet_model.dart';
 import 'package:sqflite_mvvm_design/ui/modules/home/widgets/home_list_item.dart';
 import '../../../config/router/routes.dart';
-import '../../../core/resources/color_manager.dart';
 
 class HomeScreen extends StatefulWidget {
   HomeScreen({super.key});
@@ -13,8 +12,6 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  final TextEditingController _searchController = TextEditingController();
-
   var _isInit = true;
   var _isLoading = false;
 
@@ -44,63 +41,41 @@ class _HomeScreenState extends State<HomeScreen> {
 
     PoetModel poetModel = Provider.of<PoetModel>(context, listen: true);
 
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: TextField(
-          controller: _searchController,
-          textAlign: TextAlign.right,
-          textDirection: TextDirection.rtl,
-          style: const TextStyle(color: Colors.white),
-          cursorColor: Colors.white,
-          decoration: const InputDecoration(
-            hintText: ' ... جستجو',
-            hintStyle: TextStyle(color: Colors.white54),
-            border: InputBorder.none,
-          ),
-          onChanged: (value) => poetModel.searchPoets(value),
-        ),
-      ),
-      body: _isLoading
-          ? const Center(
-        child: CircularProgressIndicator(),
-      )
-          : SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(16),
-          child: GridView.count(
-            shrinkWrap: true,
-            childAspectRatio: (itemWidth / itemHeight),
-            crossAxisCount: 2,
-            physics: const ClampingScrollPhysics(),
-            scrollDirection: Axis.vertical,
-            crossAxisSpacing: 10,
-            mainAxisSpacing: 20,
-            children: List.generate(poetModel.poets.length, (index) {
-              return HomeListItem(
-                poet: poetModel.poets[index],
-                itemHeight: itemHeight,
-                itemWidth: itemWidth,
-                onClickDelete: () {
-                  poetModel.deletePoet(poetModel.poets[index].id);
-                },
-                onClickInfo: () {
-                  AppNavigator.push(Routes.poetInfo, [(index+1).toString()]);
-                },
-                onClickEdit: () {
-                  AppNavigator.push(Routes.poetEdit, [(index+1).toString()]);
-                },
-              );
-            }),
-          ),
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => AppNavigator.push(Routes.addPoet),
-        backgroundColor: ColorManager.primary,
-        child: const Icon(Icons.add),
-      ),
-    );
-    ;
+    return _isLoading
+        ? const Center(
+            child: CircularProgressIndicator(),
+          )
+        : SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.all(16),
+              child: GridView.count(
+                shrinkWrap: true,
+                childAspectRatio: (itemWidth / itemHeight),
+                crossAxisCount: 2,
+                physics: const ClampingScrollPhysics(),
+                scrollDirection: Axis.vertical,
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 20,
+                children: List.generate(poetModel.poets.length, (index) {
+                  return HomeListItem(
+                    poet: poetModel.poets[index],
+                    itemHeight: itemHeight,
+                    itemWidth: itemWidth,
+                    onClickDelete: () {
+                      poetModel.deletePoet(poetModel.poets[index].id);
+                    },
+                    onClickInfo: () {
+                      AppNavigator.push(
+                          Routes.poetInfo, [(index + 1).toString()]);
+                    },
+                    onClickEdit: () {
+                      AppNavigator.push(
+                          Routes.poetEdit, [(index + 1).toString()]);
+                    },
+                  );
+                }),
+              ),
+            ),
+          );
   }
 }
--- lib/ui/modules/tabs/screen.dart
+++ lib/ui/modules/tabs/screen.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:sqflite_mvvm_design/core/resources/color_manager.dart';
+import 'package:sqflite_mvvm_design/ui/modules/home/screen.dart';
+
+import '../../../config/router/routes.dart';
+import '../../../providers/poet_model.dart';
 
 class TabsScreen extends StatefulWidget {
   const TabsScreen({super.key});
@@ -9,8 +14,14 @@ class TabsScreen extends StatefulWidget {
 }
 
 class _TabsScreenState extends State<TabsScreen> {
-
   int _selectedIndex = 0;
+
+  final TextEditingController _searchController = TextEditingController();
+
+  final tabs = [
+    HomeScreen(),
+    HomeScreen(),
+  ];
 
   void _onItemTapped(int index) {
     setState(() {
@@ -20,32 +31,55 @@ class _TabsScreenState extends State<TabsScreen> {
 
   @override
   Widget build(BuildContext context) {
+    PoetModel poetModel = Provider.of<PoetModel>(context, listen: true);
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('BottomNavigationBar Demo'),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        showSelectedLabels: true,
-        showUnselectedLabels: false,
-        type: BottomNavigationBarType.shifting,
-        selectedFontSize: 20,
-        selectedIconTheme: const IconThemeData(color: ColorManager.primary, size: 35),
-        unselectedIconTheme: const IconThemeData(size: 35),
-        selectedItemColor: ColorManager.primary,
-        selectedLabelStyle: const TextStyle(fontWeight: FontWeight.bold),
-        currentIndex: _selectedIndex, //New
-        onTap: _onItemTapped,
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'خانه',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.favorite),
-            label: 'علاقمندی ها',
-          ),
-        ],
-      ),
-    );;
+        appBar: AppBar(
+          centerTitle: true,
+          title: _selectedIndex == 0
+              ? TextField(
+                  controller: _searchController,
+                  textAlign: TextAlign.right,
+                  textDirection: TextDirection.rtl,
+                  style: const TextStyle(color: Colors.white),
+                  cursorColor: Colors.white,
+                  decoration: const InputDecoration(
+                    hintText: ' ... جستجو',
+                    hintStyle: TextStyle(color: Colors.white54),
+                    border: InputBorder.none,
+                  ),
+                  onChanged: (value) => poetModel.searchPoets(value),
+                )
+              : const SizedBox(),
+        ),
+        body: tabs[_selectedIndex],
+        bottomNavigationBar: BottomNavigationBar(
+          showSelectedLabels: true,
+          showUnselectedLabels: false,
+          type: BottomNavigationBarType.shifting,
+          selectedFontSize: 20,
+          selectedIconTheme:
+              const IconThemeData(color: ColorManager.primary, size: 35),
+          unselectedIconTheme: const IconThemeData(size: 35),
+          selectedItemColor: ColorManager.primary,
+          selectedLabelStyle: const TextStyle(fontWeight: FontWeight.bold),
+          currentIndex: _selectedIndex, //New
+          onTap: _onItemTapped,
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'خانه',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.favorite),
+              label: 'علاقمندی ها',
+            ),
+          ],
+        ),
+        floatingActionButton: _selectedIndex == 0 ? FloatingActionButton(
+          onPressed: () => AppNavigator.push(Routes.addPoet),
+          backgroundColor: ColorManager.primary,
+          child: const Icon(Icons.add),
+        ) : const SizedBox());
   }
 }
