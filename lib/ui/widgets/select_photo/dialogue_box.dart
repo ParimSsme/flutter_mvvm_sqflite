95dff793d68e843f55bba20bfae07c4f5cc7364a
ParimSsme <parima.ssme@gmail.com>
2023-11-17 12:30:10 +0430

    pick image from gallery and camera

--- lib/ui/widgets/select_photo/dialogue_box.dart
+++ lib/ui/widgets/select_photo/dialogue_box.dart
@@ -4,6 +4,7 @@ import 'package:sqflite_mvvm_design/config/theme/app_theme.dart';
 import 'package:sqflite_mvvm_design/core/enums/button_variant.dart';
 import 'package:sqflite_mvvm_design/core/resources/color_manager.dart';
 import 'package:sqflite_mvvm_design/ui/widgets/app_button.dart';
+import 'package:image_picker/image_picker.dart';
 
 
 Dialog AddPhotoDialogue(
@@ -13,37 +14,40 @@ Dialog AddPhotoDialogue(
   return Dialog(
     shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16.0)),
     child: SizedBox(
-      height: 200,
+      height: 240,
       child: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
-            Text("Select Photo",
+            Text("انتخاب عکس",
               style: AppTheme.light.textTheme.headline2,
               textAlign: TextAlign.left,
             ),
             const Divider(color: ColorManager.gray, height: 2),
             AppButton(
-              onPress: () {
-                _getFromGallery().then((val) {
-                  onClickTakePhoto(val);
-                });
+              onPress: () async {
+                final imageFile = await ImagePicker().pickImage(
+                  source: ImageSource.gallery,
+                  maxWidth: 600,
+                );
+                onClickTakePhoto(File(imageFile?.path ?? ''));
                 Navigator.pop(context);
               },
-              text: "PICK FROM GALLERY",
+              text: "گالری",
               variant: ButtonVariantEnum.text,
             ),
-            Container(
-              height: 40.0,
-            ),
             AppButton(
-              onPress: () {
-                _getFromCamera().then((val) {
-                  onClickTakePhoto(val);
-                });
+              onPress: () async {
+                final pickedFile = await ImagePicker().pickImage(
+                  source: ImageSource.camera,
+                  maxWidth: 1800,
+                  maxHeight: 1800,
+                );
+                onClickTakePhoto(File(pickedFile?.path ?? ''));
                 Navigator.pop(context);
               },
-              text: "PICK FROM CAMERA",
+              text: "کامره",
               variant: ButtonVariantEnum.text,
             )
           ],
@@ -51,30 +55,4 @@ Dialog AddPhotoDialogue(
       ),
     ),
   );
-}
-
-Future<File?> _getFromGallery() async {
-  // PickedFile pickedFile = await ImagePicker().getImage(
-  //   source: ImageSource.gallery,
-  //   maxWidth: 1800,
-  //   maxHeight: 1800,
-  // );
-  // if (pickedFile != null) {
-  //   return File(pickedFile.path);
-  // } else {
-  //   return null;
-  // }
-}
-
-Future<File?> _getFromCamera() async {
-  // PickedFile pickedFile = await ImagePicker().getImage(
-  //   source: ImageSource.camera,
-  //   maxWidth: 1800,
-  //   maxHeight: 1800,
-  // );
-  // if (pickedFile != null) {
-  //   return File(pickedFile.path);
-  // } else {
-  //   return null;
-  // }
 }
