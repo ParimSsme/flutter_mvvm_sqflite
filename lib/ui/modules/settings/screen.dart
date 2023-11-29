319d07773acdc301625ad09366def1babd76ea76
ParimSsme <parima.ssme@gmail.com>
2023-11-30 03:56:42 +0430

    pick a folder in settings screen

--- lib/ui/modules/settings/screen.dart
+++ lib/ui/modules/settings/screen.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'package:filesystem_picker/filesystem_picker.dart';
 import 'package:path/path.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
@@ -8,11 +9,48 @@ import 'package:sqflite_mvvm_design/ui/widgets/app_button.dart';
 import '../../../core/enums/button_variant.dart';
 import '../../../data/helper/db_conection.dart';
 
-class SettingsScreen extends StatelessWidget {
+class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
 
   static final DBConnection _databaseService = DBConnection();
-  
+
+  @override
+  State<SettingsScreen> createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+
+  Directory? rootPath;
+  String? dirPath;
+
+  @override
+  void initState() {
+    super.initState();
+    _prepareStorage();
+  }
+
+  Future<void> _prepareStorage() async {
+    rootPath = Directory('/storage/emulated/0/');
+    setState(() {});
+  }
+
+  Future<void> _pickDir(BuildContext context) async {
+    String? path = await FilesystemPicker.open(
+      title: 'Select folder',
+      context: context,
+      rootDirectory: rootPath!,
+      fsType: FilesystemType.folder,
+      pickText: 'Select this folder',
+      folderIconColor: Colors.teal,
+      requestPermission: () async =>
+      await Permission.storage.request().isGranted,
+    );
+
+    setState(() {
+      dirPath = path;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -22,6 +60,21 @@ class SettingsScreen extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
+
+            dirPath != null
+                ? Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10),
+              child: Text('$dirPath'),
+            )
+                : const Padding(
+              padding: EdgeInsets.symmetric(vertical: 10),
+              child: Text('No directory selected'),
+            ),
+            ElevatedButton(
+              child: const Text('Select directory'),
+              onPressed: (rootPath != null) ? () => _pickDir(context) : null,
+            ),
+
             AppButton(
               variant: ButtonVariantEnum.contained,
               text: 'Copy DB',
